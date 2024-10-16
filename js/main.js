2b1c8f7afe6cb0912c620621fdce6083e7a288d3
Luis Lazcano Guerrero <llazcano@dgac.gob.cl>
2024-10-16 10:04:58 -0300

    ajustesVisor

--- js/main.js
+++ js/main.js
@@ -2,36 +2,26 @@ const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const auto = true;
-const intervalTime = 4000;
+const intervalTime = 10000;
 let slideInterval;
 
 const nextSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for next slide
   if(current.nextElementSibling) {
-    // Add current to next sibling
     current.nextElementSibling.classList.add('current');
   } else {
-    // Add current to start
     slides[0].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 }
 
 const prevSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for previous slide
   if(current.previousElementSibling) {
-    // Add current to prev sibling
     current.previousElementSibling.classList.add('current');
   } else {
-    // Add current to last
     slides[slides.length - 1].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
@@ -54,8 +44,6 @@ prev.addEventListener('click', e => {
   }
 });
 
-// Auto slide
 if(auto) {
-  // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
